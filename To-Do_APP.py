5cbeb5f13fe690285b79bdfd687b9004ce5082ec
tmga-cz <142583006+tmga-cz@users.noreply.github.com>
2023-08-23 12:41:10 +0200

    Create To-Do_APP.py

---
+++ To-Do_APP.py
@@ -0,0 +1,12 @@
+import builtins
+print(dir(builtins))
+
+user_prompt = "Enter a todo:"
+
+todos = []
+ 
+while True:
+    todo = input(user_prompt)
+    print(todo.title())
+    todos.append(todo)
+
