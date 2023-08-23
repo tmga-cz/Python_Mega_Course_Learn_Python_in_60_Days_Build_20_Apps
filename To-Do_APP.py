caea3946a4fc71b01bb13c42a2f2b701928ea8d6
tmga-cz <142583006+tmga-cz@users.noreply.github.com>
2023-08-23 13:14:04 +0200

    Update To-Do_APP.py

--- To-Do_APP.py
+++ To-Do_APP.py
@@ -1,12 +1,23 @@
-import builtins
-print(dir(builtins))
-
-user_prompt = "Enter a todo:"
+#import builtins
+#print(dir(builtins))
 
 todos = []
- 
-while True:
-    todo = input(user_prompt)
-    print(todo.title())
-    todos.append(todo)
 
+while True:
+    user_action = input("Type add,show, or exit:")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for item in todos:
+                item = item.title()
+                print(item)
+        case 'exit':
+            break
+        case whatever:
+            print("Hey, you entered an unknown command")
+
+print("Bye")
