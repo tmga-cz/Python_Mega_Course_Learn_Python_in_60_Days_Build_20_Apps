923e9c1e0ff5951c2f86fbcb8fc3c1b4ba642db1
tmga-cz <142583006+tmga-cz@users.noreply.github.com>
2023-08-24 12:33:05 +0200

    Create TO-DO_App.py

---
+++ TO-DO_App/TO-DO_App.py
@@ -0,0 +1,32 @@
+#import builtins
+#print(dir(builtins))
+
+todos = []
+
+while True:
+    user_action = input("Type add, show, edit, complete or exit:")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}" #print(index, '-', item)
+                print(row)
+        case 'edit' :
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case 'complete' :
+            number = int(input("Number of the todo to complete: "))
+            todos.pop(number - 1)
+        case 'exit':
+            break
+        case whatever:
+            print("Hey, you entered an unknown command")
+
+print("Bye")
+
